3c14e6ffd4683fd6868a8ef3cf57cbaa63bc84f2
beesandpigeons <134010841+beesandpigeons@users.noreply.github.com>
2024-02-07 22:32:13 +0100

    uhhh

--- calendar/biblioteka.py
+++ calendar/biblioteka.py
@@ -6,15 +6,34 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 from bs4 import BeautifulSoup
 
-url = 'https://www.biblioteka.lodz.pl/wydarzenia/'
-response = requests.get(url)
+class Calendar():
+    def __init__(self):
+        self.SCOPES = 'https://www.googleapis.com/auth/calendar'
+        self.store = file.Storage('credentials.json')
+        self.creds = self.store.get()
+        if not self.creds or self.creds.invalid:
+            self.creds = tools.run_flow(self.flow, self.store)
+        self.service = build('calendar', 'v3', http=self.creds.authorize(Http()))
+    
+    def add(self, body):
+        events_result = self.service.events().list(calendarId='primary', 
+                                                timeMin=body['start']['dateTime'], 
+                                                timeMax=body['end']['dateTime']).execute()
+        events = events_result.get('items', [])
+        if not events:
+            self.service.events().insert(calendarId='primary', body=body).execute()
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    events = soup.find_all('div', class_='event')
-    for event in events:
-        print(event.prettify())
-        #title = event.find('h2', class_='title').text
-        #if 'DKKQ' in title:          
-else:
-    print('Failed to fetch the events page.')
+class Library():
+    url = 'https://www.biblioteka.lodz.pl/wydarzenia/'
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for event in self.events:
+            events = soup.find_all('div', class_='event')    
+            if 'DKKQ' in title:
+                date = event.find('time')['datetime']
+                calendar = Calendar()
+                calendar.add(title, date)    
+    else:
+        print('Failed to fetch the events page.')
+
