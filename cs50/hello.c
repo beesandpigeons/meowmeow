1f2a8cd1d88dcff074ba16735cfab9615fff2ec2
beesandpigeons <134010841+beesandpigeons@users.noreply.github.com>
2024-02-17 23:20:56 +0100

    a

--- cs50/hello.c
+++ cs50/hello.c
@@ -1,5 +1,8 @@
 #include <studio.h>
+#include <cs50.h>
+
 int main(void)
 {
-    printf("hello world\n");
+    string answer = get_string("What's your name? ");
+    printf("hello %s\n", answer);
 }
